a68906c8f7b3a837928df830deebc92ea19081c9
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2024-11-15 10:20:53 +0530

    final

--- Frontend/src/components/UI/Helper.js
+++ Frontend/src/components/UI/Helper.js
@@ -1,2 +1,2 @@
-//export const Base_URL= "https://expense-tracker-gogm.onrender.com"
-export const Base_URL= "http://localhost:8000"
+export const Base_URL= "https://expense-tracker-gogm.onrender.com"
+//export const Base_URL= "http://localhost:8000"
